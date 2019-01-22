141d82632d97658b41a3f10b63ef2ad2af5fbc62
David Rivero (ErCompa) <davidmriverog@gmail.com>
2019-01-22 18:08:49 -0300

    creamos los reducers

---
+++ src/app/store/reducers/reducers.ts
@@ -0,0 +1,45 @@
+import { environment } from '../../../environments/environment';
+import { ActionReducerMap, createSelector, createFeatureSelector, ActionReducer, MetaReducer, Action } from '@ngrx/store';
+import * as formRouter from '@ngrx/router-store';
+
+import { RouterStateUrl } from '../shared/utils';
+
+import { storeFreeze } from 'ngrx-store-freeze';
+
+import * as formAuth from './auth.reducer';
+
+// AppState
+export interface State {
+    auth: formAuth.State,
+    router: formRouter.RouterReducerState<RouterStateUrl>;
+}
+
+// Lists Reducers
+export const reducers: ActionReducerMap<State|any> = {
+    auth: formAuth.AuthReducer,
+    router: formRouter.routerReducer
+};
+
+// Logger
+export function logger(reducer : ActionReducer<State>) : ActionReducer<State> {
+    
+    return function(state: State, action: any): State {
+        console.log('event state', state);
+
+
+        console.log('event action', action);
+
+        return reducer(state, action);
+    }
+}
+
+export const metaReducers: MetaReducer<State>[] = ! environment.production
+    ? [logger, storeFreeze]
+    : [];
+
+export const getAuthState = createFeatureSelector<formAuth.State>('auth');
+
+export const getAuth = createSelector(
+    getAuthState,
+    formAuth.getAuthState
+);
